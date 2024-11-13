f224bf52af8569dfbbb759d0aea71e5085f85eb9
miksiuLaptop <miksiu120@gmail.com>
2024-11-13 17:48:55 +0100

    recording

--- src/main/java/pl/gda/pg/eti/kask/sa/migration/behaviours/ReceiveContainersLisBehaviour.java
+++ src/main/java/pl/gda/pg/eti/kask/sa/migration/behaviours/ReceiveContainersLisBehaviour.java
@@ -14,19 +14,12 @@ import java.util.logging.Level;
 import lombok.extern.java.Log;
 import pl.gda.pg.eti.kask.sa.migration.agents.MigratingAgent;
 
-/**
- *
- * @author psysiu
- */
 @Log
 public class ReceiveContainersLisBehaviour extends Behaviour {
 
     private boolean done = false;
-
     protected final MigratingAgent myAgent;
-
     protected final String conversationId;
-
     protected MessageTemplate mt;
 
     public ReceiveContainersLisBehaviour(MigratingAgent agent, String conversationId) {
@@ -50,15 +43,34 @@ public class ReceiveContainersLisBehaviour extends Behaviour {
                 ContentElement ce = myAgent.getContentManager().extractContent(msg);
                 jade.util.leap.List items = ((Result) ce).getItems();
                 List<Location> locations = new ArrayList<>();
+
                 items.iterator().forEachRemaining(i -> {
                     locations.add((Location) i);
                 });
-              //  locations.remove(myAgent.here());
-                myAgent.setLocations(locations);
-                myAgent.addBehaviour(new MigratingBehaviour(myAgent));
+
+                
+                Location currentLocation = myAgent.here();
+                if (locations.contains(currentLocation)) {
+                    locations.remove(currentLocation);
+                } else {
+                    log.warning("Bieżący kontener nie istnieje na liście, odświeżam listę lokalizacji...");
+                    myAgent.addBehaviour(new RequestContainersListBehaviour(myAgent));
+                    return;
+                }
+
+                if (!locations.isEmpty()) {
+                    Location nextLocation = locations.get(0);  // Wybór pierwszej dostępnej lokalizacji
+                    myAgent.setLocations(locations);  // Aktualizacja listy lokalizacji
+                    myAgent.addBehaviour(new MigratingBehaviour(myAgent));
+                } else {
+                    log.warning("Brak dostępnych kontenerów do migracji.");
+                }
+
             } catch (Codec.CodecException | OntologyException ex) {
-                log.log(Level.SEVERE, null, ex);
+                log.log(Level.SEVERE, "Błąd podczas przetwarzania wiadomości: ", ex);
             }
+        } else {
+            block();
         }
     }
 
@@ -66,5 +78,4 @@ public class ReceiveContainersLisBehaviour extends Behaviour {
     public boolean done() {
         return done;
     }
-
 }
